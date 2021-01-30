6233c6c2c92b5a84cf66ec36665edad6e78dc3c9
cw <illumea@gmail.com>
2021-01-30 16:17:03 +0800

    add create new event form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Calendar from './components/pages/Calendar'
 import EventsList from './components/calendar/EventsList'
 import Event from './components/calendar/Event'
+import NewEvent from './components/calendar/NewEvent'
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Router>
           <Switch>
             {/* <Route path="/" component={Calendar} /> */}
+            <Route path="/events/new" component={NewEvent} />
             <Route path="/events/:id" component={Event} />
             <Route path="/events" component={EventsList} />
           </Switch>
