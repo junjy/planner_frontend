138de277bdf63a1e39027fd34e426dbb20fcef7b
cw <illumea@gmail.com>
2021-02-03 12:14:05 +0800

    connect calendar to mongodb events

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+// Test only
 // import TUICalendarTest from './components/pages/TUICalendarTest'
 import FullCalendarTest from './components/pages/FullCalendarTest'
-import FullCalTestEvents from './components/pages/FullCalTestEvents'
 
+// Actual 
 import EventsList from './components/calendar/EventsList'
 import Event from './components/calendar/Event'
 import NewEvent from './components/calendar/NewEvent'
@@ -22,7 +24,6 @@ class App extends React.Component {
             <Route path="/events" component={EventsList} />
             {/* <Route exact path="/calendar" component={TUICalendarTest} /> */}
             <Route exact path="/calendar" component={FullCalendarTest} />
-            <Route exact path="/calevents" component={FullCalTestEvents} />
           </Switch>
         </Router>
       </div>
---
+++ src/components/notused/EventsList2.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import plannerAPI from '../../services/api'
+
+class Events extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: [],
+            // text: null,
+        }
+    }
+
+    componentDidMount() {
+
+        plannerAPI.listEvents().then((response) => {
+            // console.log(response.data.events);
+
+            this.setState({
+                events: response.data.events,
+                // text: "calendar component mounted at 3pm"
+            });
+        }).catch(err => {
+            console.log(err)
+        })
+
+    }
+
+
+    render() {
+        return(
+            <div className="page-events">
+                <div className="container">
+                    <h1>My Calendar: All Events</h1>
+                    {/* <p>Text: { this.state.text }</p> */}
+                    { 
+                        this.state.events.length > 0 ? (
+                            this.state.events.map(element => {
+                                return (
+                                    <div className="events-display">
+                                        <p><strong>Title: {element.title}</strong></p>
+                                        <p>Start: {element.start}</p>
+                                        <p>End: {element.end}</p>
+                                        <p>EventID: {element._id}</p>
+                                        <br />
+                                    </div>
+                                )
+                            })
+                        ) : ''
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Events;
